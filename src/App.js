efd38c886d69a54b3eeb35ecb12ea4d92a027902
Skender Ademi <skender@giggle.tips>
2020-10-16 12:00:14 +0200

    Add header and auth components

--- src/App.js
+++ src/App.js
@@ -5,21 +5,19 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import AuthPage from './pages/auth/auth.component';
 
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import Header from './layouts/header/header.component';
 
 function App() {
 
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/auth' component={AuthPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/auth/auth.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './auth.styles.scss';
+
+import SignIn from '../../components/sign-in/sign-in.component'
+
+
+const authPage = () => (
+    <div className="auth">SIGN IN
+        <SignIn/>
+    </div>
+)
+
+export default authPage;
